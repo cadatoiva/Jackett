410b0589cdde2cbf8c8785d76e25dbf40d16c97e
Cory <cadatoiva@gmail.com>
2020-03-14 18:58:50 -0500

    core: change byte result name to ContentBytes in preparation for merge (#7653)

--- src/Jackett.Common/Indexers/LostFilm.cs
+++ src/Jackett.Common/Indexers/LostFilm.cs
@@ -122,7 +122,7 @@ namespace Jackett.Common.Indexers
             {
                 var captchaUrl = SiteLink + qCaptchaImg.GetAttribute("src");
                 var captchaImage = await RequestBytesWithCookies(captchaUrl, loginPage.Cookies);
-                configData.CaptchaImage.Value = captchaImage.Content;
+                configData.CaptchaImage.Value = captchaImage.ContentBytes;
             }
             else
             {
